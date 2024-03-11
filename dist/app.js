b9326b30512504459cddd444ceb01db25779593b
Dominik Cisár <dominik.cisar@gmail.com>
2024-03-11 03:12:27 +0100

    deleted dist

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Fetching and rendering logic goes here
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("data.json")
-        .then((response) => response.json())
-        .then((data) => {
-        console.log(data); // Implement rendering logic based on your app's design
-    });
-});
