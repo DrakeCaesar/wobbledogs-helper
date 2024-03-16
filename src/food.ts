0aeeec57690c2731fa319f79d3e37f19d2999314
DrakeCaesar <dominik.cisar@gmail.com>
2024-03-16 13:26:08 +0100

    added new food and ordered

--- src/food.ts
+++ src/food.ts
@@ -4,29 +4,81 @@ export interface Food {
   source?: string;
 }
 
-export const unknown: Food = {
-  name: "Unknown",
-  image: "unknown",
-};
-export const chip: Food = {
-  name: "Chip",
-  image: "chip",
-};
-export const nutritionalPellet: Food = {
-  name: "Nutritional Pellet",
-  image: "nutritional-pellet",
+export const alienFruit: Food = {
+  name: "Alien Fruit",
+  image: "alien-fruit",
 };
 export const appleSlice: Food = {
   name: "Apple Slice",
   image: "apple-slice",
 };
+export const babyTooth: Food = {
+  name: "Baby Tooth",
+  image: "baby-tooth",
+};
+export const banana: Food = {
+  name: "Banana",
+  image: "banana",
+};
+export const boxOfRaisins: Food = {
+  name: "Box of Raisins",
+  image: "box-of-raisins",
+};
+export const burrito: Food = {
+  name: "Burrito",
+  image: "burrito",
+};
+export const candiedApple: Food = {
+  name: "Candied Apple",
+  image: "candied-apple",
+};
+export const candyCane: Food = {
+  name: "Candy Cane",
+  image: "candy-cane",
+};
+export const candyCorn: Food = {
+  name: "Candy Corn",
+  image: "candy-corn",
+};
+export const cheeseBall: Food = {
+  name: "Cheese Ball",
+  image: "cheese-ball",
+};
+export const chickenNugget: Food = {
+  name: "Chicken Nugget",
+  image: "chicken-nugget",
+};
+export const chip: Food = {
+  name: "Chip",
+  image: "chip",
+};
+export const coconut: Food = {
+  name: "Coconut",
+  image: "coconut",
+};
+export const crackedCore: Food = {
+  name: "Cracked Core",
+  image: "cracked-core",
+};
 export const cutFruit: Food = {
   name: "Cut Fruit",
   image: "cut-fruit",
 };
-export const babyTooth: Food = {
-  name: "Baby Tooth",
-  image: "baby-tooth",
+export const decorativeCorn: Food = {
+  name: "Decorative Corn",
+  image: "decorative-corn",
+};
+export const dehydratedTreat: Food = {
+  name: "Dehydrated Treat",
+  image: "dehydrated-treat",
+};
+export const deviledEgg: Food = {
+  name: "Deviled Egg",
+  image: "deviled-egg",
+};
+export const dirtClump: Food = {
+  name: "Dirt Clump",
+  image: "dirt-clump",
 };
 export const dogBody: Food = {
   name: "Dog Body",
@@ -48,141 +100,135 @@ export const dogWing: Food = {
   name: "Dog Wing",
   image: "dog-wing",
 };
-export const garlicBread: Food = {
-  name: "Garlic Bread",
-  image: "garlic-bread",
-};
-export const pancake: Food = {
-  name: "Pancake",
-  image: "pancake",
-};
-export const burrito: Food = {
-  name: "Burrito",
-  image: "burrito",
-};
-export const coconut: Food = {
-  name: "Coconut",
-  image: "coconut",
-};
-export const cheeseBall: Food = {
-  name: "Cheese Ball",
-  image: "cheese-ball",
-};
 export const emptyCocoon: Food = {
   name: "Empty Cocoon",
   image: "empty-cocoon",
-};
-export const dirtClump: Food = {
-  name: "Dirt Clump",
-  image: "dirt-clump",
-};
-export const hamSlider: Food = {
-  name: "Ham Slider",
-  image: "ham-slider",
-};
-export const moonCheese: Food = {
-  name: "Moon Cheese",
-  image: "moon-cheese",
-};
-export const dehydratedTreat: Food = {
-  name: "Dehydrated Treat",
-  image: "dehydrated-treat",
-};
-export const fruitCake: Food = {
-  name: "Fruit Cake",
-  image: "fruit-cake",
-};
-export const candyCane: Food = {
-  name: "Candy Cane",
-  image: "candy-cane",
-};
-export const alienFruit: Food = {
-  name: "Alien Fruit",
-  image: "alien-fruit",
-};
-export const lasagna: Food = {
-  name: "Lasagna",
-  image: "lasagna",
-};
-export const pizzaBagel: Food = {
-  name: "Pizza Bagel",
-  image: "pizza-bagel",
-};
-export const onionRing: Food = {
-  name: "Onion Ring",
-  image: "onion-ring",
-};
-export const chickenNugget: Food = {
-  name: "Chicken Nugget",
-  image: "chicken-nugget",
 };
 export const frenchFry: Food = {
   name: "French Fry",
   image: "french-fry",
 };
-export const banana: Food = {
-  name: "Banana",
-  image: "banana",
+export const fruitCake: Food = {
+  name: "Fruit Cake",
+  image: "fruit-cake",
 };
-export const honeyComb: Food = {
-  name: "Honey Comb",
-  image: "honey-comb",
+export const garlicBread: Food = {
+  name: "Garlic Bread",
+  image: "garlic-bread",
+};
+export const garlicBulb: Food = {
+  name: "Garlic Bulb",
+  image: "garlic-bulb",
+};
+export const gourd: Food = {
+  name: "Gourd",
+  image: "gourd",
 };
 export const halfEatenFood: Food = {
   name: "Half Eaten Food",
   image: "half-eaten-food",
 };
+export const hamSlider: Food = {
+  name: "Ham Slider",
+  image: "ham-slider",
+};
+export const honeyComb: Food = {
+  name: "Honey Comb",
+  image: "honey-comb",
+};
+export const lasagna: Food = {
+  name: "Lasagna",
+  image: "lasagna",
+};
+export const miniPumpkin: Food = {
+  name: "Mini Pumpkin",
+  image: "mini-pumpkin",
+};
+export const moonCheese: Food = {
+  name: "Moon Cheese",
+  image: "moon-cheese",
+};
+export const nutritionalPellet: Food = {
+  name: "Nutritional Pellet",
+  image: "nutritional-pellet",
+};
+export const onionRing: Food = {
+  name: "Onion Ring",
+  image: "onion-ring",
+};
+export const pancake: Food = {
+  name: "Pancake",
+  image: "pancake",
+};
+export const pizzaBagel: Food = {
+  name: "Pizza Bagel",
+  image: "pizza-bagel",
+};
 export const poop: Food = {
   name: "Poop",
   image: "poop",
 };
-export const snowball: Food = {
-  name: "Snowball",
-  image: "snowball",
-};
-export const crackedCore: Food = {
-  name: "Cracked Core",
-  image: "cracked-core",
+export const pumpkinPie: Food = {
+  name: "Pumpkin Pie",
+  image: "pumpkin-pie",
 };
 export const sampleCup: Food = {
   name: "Sample Cup",
   image: "sample-cup",
 };
+export const snowball: Food = {
+  name: "Snowball",
+  image: "snowball",
+};
+export const unknown: Food = {
+  name: "Unknown",
+  image: "unknown",
+};
 
 export const foods: Food[] = [
-  chip,
-  nutritionalPellet,
+  alienFruit,
   appleSlice,
-  cutFruit,
   babyTooth,
+  banana,
+  boxOfRaisins,
+  burrito,
+  candiedApple,
+  candyCane,
+  candyCorn,
+  cheeseBall,
+  chickenNugget,
+  chip,
+  coconut,
+  crackedCore,
+  cutFruit,
+  decorativeCorn,
+  dehydratedTreat,
+  deviledEgg,
+  dirtClump,
   dogBody,
   dogHead,
   dogLeg,
   dogTail,
   dogWing,
-  garlicBread,
-  pancake,
-  burrito,
-  coconut,
-  cheeseBall,
   emptyCocoon,
-  dirtClump,
-  hamSlider,
-  moonCheese,
-  dehydratedTreat,
-  fruitCake,
-  candyCane,
-  alienFruit,
-  lasagna,
-  pizzaBagel,
-  onionRing,
-  chickenNugget,
   frenchFry,
-  banana,
-  honeyComb,
+  fruitCake,
+  garlicBread,
+  garlicBulb,
+  gourd,
   halfEatenFood,
+  hamSlider,
+  honeyComb,
+  lasagna,
+  miniPumpkin,
+  moonCheese,
+  nutritionalPellet,
+  onionRing,
+  pancake,
+  pizzaBagel,
   poop,
-  snowball,
-  crackedCore,
+  pumpkinPie,
   sampleCup,
+  snowball,
+  unknown,
 ];
